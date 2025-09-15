e8502f99671fd9fe189eff47c638bc8e3f769b45
Talles <talves2503@gmail.com>
2025-09-15 11:56:01 -0300

    fix: cancel import from buttonprops interface

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
