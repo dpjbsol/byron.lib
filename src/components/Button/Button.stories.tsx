d946b65354fffdf1745eefb0cfb8752a71d15f61
Talles <talves2503@gmail.com>
2025-09-15 12:30:12 -0300

    feat: storybook setup

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from ".";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    children: {
+      control: "text",
+      description: "The content of the button",
+    },
+    onClick: { action: "clicked" },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: "Button",
+    onClick: () => console.log("teste"),
+  },
+};
